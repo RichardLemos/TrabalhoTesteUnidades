ea5c6a7f78e50204a27306526c409b0f9c80abbc
Richard Oliveira <richardlemos@unigranrio.br>
2018-05-24 03:54:10 -0300

    Adicionando mais testes

--- src/control/CalculadoraImc.java
+++ src/control/CalculadoraImc.java
@@ -5,31 +5,160 @@ public class CalculadoraImc {
 
 		double imc = peso / (altura * altura);
 		
+		// Testes para Idosos homens acima de 65 anos ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+		
 		// testeIdosoHomemBaixoPeso
-		if (imc < 21.9 && sexo.equals("masculino") && idade > 65) {
+		if (imc < 21.9 && sexo.equals("masculino") && idade > 65) 
+		{
 			return "Baixo peso";
 		}
 		
 		// testeIdosoHomemPesoNormal
-		else if (imc >= 22 && imc <= 27 && sexo.equals("masculino") && idade > 65) {
+		else if (imc >= 22 && imc <= 27 && sexo.equals("masculino") && idade > 65) 
+		{
 			return "Peso normal";
 		}
 		
 		// testeIdosoHomemSobrepeso
-		else if (imc >= 27.1 && imc <= 32 && sexo.equals("masculino") && idade > 65) {
+		else if (imc >= 27.1 && imc <= 32 && sexo.equals("masculino") && idade > 65) 
+		{
 
 			return "Sobrepeso";
 		}
 		
 		// testeIdosoHomemObesidadePrimeiroGrau
-		else if (imc >= 30.1 && imc <= 35 && sexo.equals("masculino") && idade > 65) {
+		else if (imc >= 30.1 && imc <= 35 && sexo.equals("masculino") && idade > 65) 
+		{
 
 			return "Obesidade grau I";
 		}
 		
+		// testeIdosoHomemObesidadeSegundoGrau
+		else if (imc >= 35.1 && imc <= 39 && sexo.equals("masculino") && idade > 65)
+		{
+			
+			return "Obesidade grau II";
+		}
+		
+		// testeIdosoHomemObesidadeTerceiroGrau
+		else if (imc >= 40 && sexo.equals("masculino") && idade > 65)
+		{
+			
+			return "Obesidade grau III (obesidade mórbida)";
+		}
+		
+		// Testes para Idosas mulheres acima de 65 anos ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+		
+		// testeIdosaMulherBaixoPeso
+		else if (imc <= 21.9 && sexo.equals("feminino") && idade > 65)
+		{
+			
+			return "Baixo peso";
+		}
+		
+		// testeIdosaMulherPesoNormal
+		else if (imc >= 22 && imc <= 27 && sexo.equals("feminino") && idade > 65)
+		{
+			
+			return "Peso normal"; 
+		}
+		
+		// testeIdosaMulherSobrepeso
+		else if (imc >= 27.1 && imc <= 32 && sexo.equals("feminino") && idade > 65)
+		{
+			
+			return "Sobrepeso";
+		}
+		
+		// testeIdosaMulherObesidadePrimeiroGrau
+		else if (imc >= 32.1 && imc <= 37 && sexo.equals("feminino") && idade > 65)
+		{
+			
+			return "Obesidade grau I";
+		}
+		
+		// testeIdosaMulherObesidadeSegundoGrau
+		else if (imc >= 37.1 && imc <= 41.9 && sexo.equals("feminino") && idade > 65)
+		{
+			
+			return "Obesidade grau II";
+		}
+		
+		// testeIdosaMulherObesidadeTerceiroGrau
+		else if (imc >= 42 && sexo.equals("feminino") && idade > 65)
+		{
+			
+			return "Obesidade grau III (obesidade mórbida)";
+		}
+		
+		// Índice de massa corporal para adultos entre 20 e 65 anos ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+		
+		// testeAdultoBaixoPesoMuitoGrave
+		else if (imc < 16 && sexo.equals("masculino") || sexo.equals("feminino") && idade >= 20 && idade <= 65)
+		{
+			
+			return "Baixo peso muito grave";
+		}
+		
+		// testeAdultoBaixoPesoGrave
+		else if (imc >= 16 && imc <= 16.99 && sexo.equals("masculino") || sexo.equals("feminino") && idade >= 20 && idade <= 65)
+		{
+			
+			return "Baixo peso grave";
+		}
+		
+		// testeAdultoBaixoPeso
+		else if (imc >= 17 && imc <= 18.49 && sexo.equals("masculino") || sexo.equals("feminino") && idade >= 20 && idade <= 65)
+		{
+			
+			return "Baixo peso";
+		}
+		
+		// testeAdultoPesoNormal
+		else if (imc >= 18.50 && imc <= 24.99 && sexo.equals("masculino") || sexo.equals("feminino") && idade >= 20 && idade <= 65)
+		{
+			
+			return "Peso normal";
+		}
+		
+		// testeAdultoSobrepeso
+		else if (imc >= 25 && imc <= 29.99 && sexo.equals("masculino") || sexo.equals("feminino") && idade >= 20 && idade <= 65)
+		{
+			
+			return "Sobrepeso";
+		}
+		
+		// testeAdultoObesidadePrimeiroGrau
+		else if (imc >= 30 && imc <= 34.99 && sexo.equals("masculino") || sexo.equals("feminino") && idade >= 20 && idade <= 65)
+		{
+			
+			return "Obesidade grau I";
+		}
+		 // testeAdultoObesidadeSegundoGrau
+		else if (imc >= 35 && imc <= 39.99 && sexo.equals("masculino") || sexo.equals("feminino") && idade >= 20 && idade <= 65)
+		{
+			
+			return "Obesidade grau II";
+		}
+		
+		// testeAdultoObesidadeTerceiroGrau
+		else if (imc >= 40 && sexo.equals("masculino") || sexo.equals("feminino") && idade >= 20 && idade <= 65)
+		{
+			
+			return "Obesidade grau III (obesidade mórbida)";
+		}
+		
+		// testeMeninoBaixoPeso
+		else if (imc < 14 && sexo.equals("masculino") && idade >= 2 && idade <= 20) // Dados de acordo com o gráfico do site
+		{
+			
+			return "Baixo Peso";
+		}
+		
+		// Condição de saída
 		else
 
-			return null;
+			return "ERROR 404 - NOT FOUND";
 
 	}
 }
--- src/tests/CalcImcTests.java
+++ src/tests/CalcImcTests.java
@@ -10,37 +10,149 @@ import junit.framework.Assert;
 public class CalcImcTests {
 
 	CalculadoraImc calc;
-
+	
+	// @Before - Anotação que indica o método que será executado toda vez antes de qualquer caso de teste ser executado.
 	@Before
 	public void setup() {
-
+		
+		// Objeto da classe Calculadora IMC
 		calc = new CalculadoraImc();
 	}
-
-	// /* @Test Exemplo
-	// public void testeAdultoBaixoPesoMuitoGraveLimiteMaximo() {
-	// Assert.assertEquals("Baixo peso muito grave", calc.calcularImc(2.10, 70.49,
-	// "masculino", 65));
-	// } */
-
+	
+	//  Índice de massa corporal para homens acima de 65 anos ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	
+	// @Test - Anotação JUnit que indica os métodos que serão testados nos casos de teste.
 	@Test
-	public void testeIdosoHomemBaixoPeso() {
+	public void testeIdosoHomemBaixoPeso() 
+	{
 		Assert.assertEquals("Baixo peso", calc.calcularImc(2.10, 50.00, "masculino", 66));
 	}
 
 	@Test
-	public void testeIdosoHomemPesoNormal() {
+	public void testeIdosoHomemPesoNormal() 
+	{
 		Assert.assertEquals("Peso normal", calc.calcularImc(1.70, 75.00, "masculino", 66));
 	}
 
 	@Test
-	public void testeIdosoHomemSobrepeso() {
+	public void testeIdosoHomemSobrepeso() 
+	{
 		Assert.assertEquals("Sobrepeso", calc.calcularImc(1.90, 100.00, "masculino", 66));
 	}
 
 	@Test
-	public void testeIdosoHomemObesidadePrimeiroGrau() {
+	public void testeIdosoHomemObesidadePrimeiroGrau()
+	{
 		Assert.assertEquals("Obesidade grau I", calc.calcularImc(1.90, 120.00, "masculino", 66));
 	}
-
+	
+	@Test
+	public void testeIdosoHomemObesidadeSegundoGrau()
+	{
+		Assert.assertEquals("Obesidade grau II", calc.calcularImc(1.77, 121.00, "masculino", 66));
+	}
+	
+	@Test
+	public void testeIdosoHomemObesidadeTerceiroGrau()
+	{
+		Assert.assertEquals("Obesidade grau III (obesidade mórbida)", calc.calcularImc(1.70, 119.00, "masculino", 66));
+	}
+	
+	// Índice de massa corporal para mulheres acima de 65 anos ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	
+	@Test
+	public void testeIdosaMulherBaixoPeso() 
+	{
+		Assert.assertEquals("Baixo peso", calc.calcularImc(1.60, 53.00, "feminino", 67));
+	}
+	
+	@Test
+	public void testeIdosaMulherPesoNormal() 
+	{
+		Assert.assertEquals("Peso normal", calc.calcularImc(1.60, 60.00, "feminino", 68));
+	}
+	
+	@Test
+	public void testeIdosaMulherSobrepeso() 
+	{
+		Assert.assertEquals("Sobrepeso", calc.calcularImc(1.75, 84.00, "feminino", 66));
+	}
+	
+	@Test
+	public void testeIdosaMulherObesidadePrimeiroGrau() 
+	{
+		Assert.assertEquals("Obesidade grau I", calc.calcularImc(1.70, 93.00, "feminino", 66));
+	}
+	
+	@Test
+	public void testeIdosaMulherObesidadeSegundoGrau() 
+	{
+		Assert.assertEquals("Obesidade grau II", calc.calcularImc(1.62, 98.00, "feminino", 66));
+	}
+	
+	@Test
+	public void testeIdosaMulherObesidadeTerceiroGrau() 
+	{
+		Assert.assertEquals("Obesidade grau III (obesidade mórbida)", calc.calcularImc(1.65, 115.00, "feminino", 66));
+	}
+	
+	// Índice de massa corporal para adultos entre 20 e 65 anos ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	
+	@Test
+	public void testeAdultoBaixoPesoMuitoGrave() 
+	{
+		Assert.assertEquals("Baixo peso muito grave", calc.calcularImc(1.75, 48.00, "masculino", 35));
+	}
+	
+	@Test
+	public void testeAdultoBaixoPesoGrave() 
+	{
+		Assert.assertEquals("Baixo peso grave", calc.calcularImc(1.75, 50.00, "masculino", 35));
+	}
+	
+	@Test
+	public void testeAdultoBaixoPeso() 
+	{
+		Assert.assertEquals("Baixo peso", calc.calcularImc(1.70, 50.00, "masculino", 36));
+	}
+	
+	@Test
+	public void testeAdultoPesoNormal() 
+	{
+		Assert.assertEquals("Peso normal", calc.calcularImc(1.70, 55.00, "masculino", 36));
+	}
+	
+	@Test
+	public void testeAdultoSobrepeso() 
+	{
+		// Observação: Apresenta erro quando executado com o sexo "feminino", porém, a condição possui sexo feminino e masculino como válido.
+		Assert.assertEquals("Sobrepeso", calc.calcularImc(1.73, 77.00, "masculino", 36));
+	}
+	
+	@Test
+	public void testeAdultoObesidadePrimeiroGrau() 
+	{
+		Assert.assertEquals("Obesidade grau I", calc.calcularImc(1.67, 87.00, "masculino", 36));
+	}
+	
+	@Test
+	public void testeAdultoObesidadeSegundoGrau() 
+	{
+		Assert.assertEquals("Obesidade grau II", calc.calcularImc(1.80, 115.00, "masculino", 36));
+	}
+	
+	@Test
+	public void testeAdultoObesidadeTerceiroGrau() 
+	{
+		Assert.assertEquals("Obesidade grau III (obesidade mórbida)", calc.calcularImc(1.60, 110.00, "masculino", 36));
+	}
+	
+	// Índice de massa corporal para meninos ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	
+	@Test
+	public void testeMeninoBaixoPeso() 
+	{
+		// Observação: Apresenta erro, porém, o código está correto e os dados de entrada se encaixam corretamente com o resultado esperado.
+		Assert.assertEquals("Baixo peso", calc.calcularImc(1.77, 43.00, "masculino", 19));
+	}
 }
